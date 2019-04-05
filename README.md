16e2d09e4ee17b8f2bcb4aafe7f65940c548e393
Enrico Cambiaso <enrico.cambiaso@gmail.com>
2019-04-05 14:53:06 +0200

    Upload of previous version

---
+++ README.md
@@ -0,0 +1,98 @@
+# cookidump
+
+Easily dump cookidoo recipes from the official website
+
+### Description ###
+
+This program allows you to dump all recipes on [Cookidoo](https://cookidoo.co.uk) websites (available for different countries) for offline and posticipate reading.
+Those recipes are valid in particular for [Thermomix/Bimby](https://en.wikipedia.org/wiki/Thermomix) devices.
+In order to dump the recipes, a valid subscription is needed.
+
+The main program is based on [jakubszalaty/cookidoo-parser](https://github.com/jakubszalaty/cookidoo-parser).
+
+### Installation ###
+
+
+1. Clone the repository:
+
+```
+git clone https://github.com/auino/cookidump.git
+```
+
+2. `cd` into the download folder
+
+3. You are ready to dump your recipes
+
+### Usage ###
+
+The following syntax is allowed:
+
+```
+python cookidump.py <domain> <outputdir> <authorization> [-c <recipescount>]
+```
+
+where:
+* `domain` identifies the cookidoo domain to be used (e.g. [cookidoo.it](http://cookidoo.it) for the Italian domain)
+* `outputdir` identifies the output directory (avoid the `~` character identifying your home directoy): a `CookiDump.zip` file will be generated inside of such directory
+* `authorization` identifies the authorization bearer to use for the communication (see below)
+
+#### Get your authorization bearer ####
+
+In order to dump your recipes, you have to provide an authorization bearer/token to the script communicating with the Cookidoo website.
+This field identifies an authorization token associated to your account in the platform (a valid subscription is required in order to retrieve a valid token).
+
+In order to get the authorization bearer you have to:
+
+1. Open the Cookidoo website (e.g. [cookidoo.it](http://cookidoo.it)) from your browser
+
+2. Log into the platform with your credentials
+
+3. Follow the instructions in the following GIF image (in Italian language, valid for [Safari](https://www.apple.com/safari/), but similar options are available on other browsers)
+
+![Steps to get Your Authorization Bearer](authorizationbearer.gif)
+
+4. Check that the string starts with the `Bearer` word (in case, remove the `Authorization ` substring)
+
+5. Run the command, by passing the `authorization` parameter as a single parameter (hence, between `"` characters)
+
+6. After the program execution, an output `CookiDump.zip` file will be produced, containing all dumped recipes (see [sample directory](https://github.com/auino/cookidump/tree/master/sample_output) for human readable output samples)
+
+#### Sample usage ####
+
+Here is a sample usage of the tool:
+
+```
+python cookidump.py -c 10 cookidoo.it /tmp/cookidump "Bearer eyJ...ZAw"
+```
+
+where:
+* `-c 10` specifies that we want to retrieve `10` recipes
+* `cookidoo.it` specifies the domain to be used (Italian domain in this sample)
+* `/tmp/cookidump` specifies the output (temporary) directory (it will be created, if not already existent)
+* `"Bearer eyJ...ZAw"` (cutted) specifies the authorization bearer to be used 
+
+A correct execution of the program will produce a `CookiDump.zip` file, including the files reported in the [sample directory](https://github.com/auino/cookidump/tree/master/sample_output) (JSON files have been converted to a human readable version).
+
+Note that contents in the [sample directory](https://github.com/auino/cookidump/tree/master/sample_output) have been anonimized: due to copyrights of the original creators, full recipes details can not be shared.
+Nevertheless, the directory contains valuable information concerning the adopted data structures.
+
+It follows the adopted anonimization command (based on [a post on StackOverflow](https://stackoverflow.com/questions/26281538/find-and-replace-for-json-with-sed-or-awk)]:
+
+```
+cat $FILE \
+| sed 's/\("id":"\)[^"]*\("\)/\1REPLACED_ID\2/g' \
+| sed 's/\("v1Id":"\)[^"]*\("\)/\1REPLACED_V1ID\2/g' \
+| sed 's/\("href":"\)[^"]*\("\)/\1REPLACED_HREF\2/g' \
+| sed 's/\("url":"\)[^"]*\("\)/\1REPLACED_URL\2/g' \
+| sed 's/\("image":"\)[^"]*\("\)/\1REPLACED_IMAGE\2/g' \
+| sed 's/\("imageUrl":"\)[^"]*\("\)/\1REPLACED_IMAGEURL\2/g' \
+| sed 's/\("locale":"\)[^"]*\("\)/\1REPLACED_LOCALE\2/g'
+```
+
+### TODO ###
+
+* Parse downloaded recipes to store them on a database or to generate readable output files (e.g. HTML, PDF, etc.)
+
+### Contacts ###
+
+You can find me on Twitter as [@auino](https://twitter.com/auino).
---
+++ cookidump.py
@@ -0,0 +1,159 @@
+#!/bin/python
+
+import os
+import re
+import sys
+import json
+import shutil
+import urllib2
+import argparse
+
+# arguments parsing
+parser = argparse.ArgumentParser(description='Dump Cookidoo recipes')
+parser.add_argument('domain', type=str, help='the cookidoo domain to be used (e.g. cookidoo.it)')
+parser.add_argument('outputdir', type=str, help='the output directory')
+parser.add_argument('-c', '--recipescount', type=int, help='the number of recipes to dump', default=1455)
+parser.add_argument('authorization', type=str, help='the authorization bearer (see https://github.com/auino/cookidump for more information)')
+args = parser.parse_args()
+
+# arguments retrieval
+DOMAIN = args.domain
+BEARER = args.authorization
+OUTPUTDIR = args.outputdir
+LIMIT = args.recipescount
+OUTPUTFILE = OUTPUTDIR+('/' if OUTPUTDIR[-1:] != '/' else '')+'CookiDump'
+
+# other internal variables
+COLLECTIONSLIMIT = 1000
+
+# useful variables creation
+TMPDIR = '/tmp/cookidump/'
+BASEURL = 'https://'+DOMAIN
+BROWSEURL = BASEURL+'/vorwerkApiV2/apiv2/browseRecipe?limit='+str(LIMIT)
+COLLECTIONSLISTURL = BASEURL+'/vorwerkApiV2/apiv2/recipeCollection?searchTerm=&sort=0&limit='+str(COLLECTIONSLIMIT)
+RECIPEBASEURL = BASEURL+'/vorwerkApiV2/apiv2/recipe/'
+
+# gets the image of a recipe (the biggest image possible)
+def getrecipeimage(r):
+	needle = 'ipad_recipe_thumb_large'
+	res = None
+	for i in r['imageSlots']:
+		res = i['url']
+		if i['name'] == needle: return res
+	return res
+
+# gets the id of a recipe
+def getrecipeid(r):
+	try:
+		url = r['links'][0]['href']+'/'
+		res = re.search('/browseRecipe/(.+)/', url, flags=re.IGNORECASE).group(1)
+		if '/' in res: res = res[:res.index('/')]
+		return res
+	except Exception, e:
+		print str(e)
+		return None
+
+# generates the recipe url based on the recipe id
+def getrecipeurl(r):
+	recipeid = getrecipeid(r)
+	return RECIPEBASEURL+recipeid
+
+# gets recipe times
+def getrecipetime(r, needle, field='value'):
+	for t in r['times']:
+		try:
+			if t['type'].upper() == needle.upper(): return t[field]
+		except: pass
+	return None
+
+# downloads information from the cookidoo website
+def geturldata(url):
+	try:
+		headers = {"Authorization": BEARER}
+		request = urllib2.Request(url, headers=headers)
+		contents = urllib2.urlopen(request).read()
+		res = json.loads(contents)
+		return res
+	except Exception, e:
+		if e.code == 401: print 'You are not authorized to get recipes.\nPlease try updating your authorization bearer (see https://github.com/auino/cookidump for more information).'
+		else: print str(e)
+		exit(2)
+
+# writes data in filename
+def writetofile(data, filename):
+	out_file = open(filename, 'w')
+	out_file.write(data)
+	out_file.close()
+
+# gets the collections list
+def getcollectionslist():
+	return geturldata(COLLECTIONSLISTURL)
+
+# gets the recipes list
+def getrecipeslist():
+	return geturldata(BROWSEURL)
+
+# gets the recipe given an input url
+def getrecipe(url):
+	return geturldata(url)
+
+# the list of all found collections
+collections = getcollectionslist()
+
+# the list of all found recipes
+recipes = []
+
+# cycling on found recipes
+for recipe in getrecipeslist()['content']:
+	# generating the recipe object
+	recipe_obj = {
+		'id': getrecipeid(recipe),
+		'name': recipe['name'],
+		'image': getrecipeimage(recipe),
+		'url': getrecipeurl(recipe),
+		'v1Id': recipe['v1Id'],
+		'portion': recipe['portion']['value'],
+		'times': {
+			'total': getrecipetime(recipe, 'total_time'),
+			'active': getrecipetime(recipe, 'active_time'),
+			'waiting': getrecipetime(recipe, 'waiting_time'),
+			'baking': getrecipetime(recipe, 'baking_time'),
+			'baking_from': getrecipetime(recipe, 'baking_time', field='from'),
+			'baking_to': getrecipetime(recipe, 'baking_time', field='to'),
+			'thermomix': getrecipetime(recipe, 'thermomix_time')
+		},
+		'fulldata': recipe
+	}
+	# adding the recipe object to the recipes list
+	recipes.append(recipe_obj)
+
+# output file generation
+
+# removing the temporary directory, if already existent
+try: os.remove(TMPDIR)
+except: pass
+
+# creating the temporary directory again
+if not os.path.exists(TMPDIR): os.makedirs(TMPDIR)
+
+# storing the collections list
+writetofile(json.dumps(collections), TMPDIR+'collections.json')
+
+# storing the recipes list
+writetofile(json.dumps(recipes), TMPDIR+'recipes.json')
+
+# cycling on found recipes
+for r in recipes:
+	# getting recipe details
+	r['recipe'] = getrecipe(r['url'])
+	# writing the recipe as a json file
+	writetofile(json.dumps(r), TMPDIR+'recipe_'+r['id']+'.json')
+
+# removing the output zip file, if already existent
+if os.path.isfile(OUTPUTFILE): os.remove(OUTPUTFILE)
+
+# creating the output zip file
+shutil.make_archive(OUTPUTFILE, 'zip', TMPDIR)
+
+# removing the temporary directory
+shutil.rmtree(TMPDIR)
