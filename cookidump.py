f51f3e3151ae35bdecdde61e7b36cf0755c08e23
mrwogu <mail@wogu.pl>
2021-05-08 20:24:37 +0200

    feat: write more details to json

--- cookidump.py
+++ cookidump.py
@@ -66,10 +66,19 @@ def recipeToJSON(browser, recipeID):
 
 	recipe = {}
 	recipe['id'] = recipeID
+	recipe['language'] = soup.select_one('html').attrs['lang']
 	recipe['title'] = soup.select_one(".recipe-card__title").text
+	recipe['rating_count'] = re.sub(r'\D', '', soup.select_one(".core-rating__label").text, flags=re.IGNORECASE)
+	recipe['rating_score'] = soup.select_one(".core-rating__counter").text
+	recipe['tm-versions'] = [v.text.replace('\n','').strip().lower() for v in soup.select(".recipe-card__tm-version core-badge")]
 	recipe.update({ l.text : l.next_sibling.strip() for l in soup.select("core-feature-icons label span") })
 	recipe['ingredients'] = [re.sub(' +', ' ', li.text).replace('\n','').strip() for li in soup.select("#ingredients li")]
+	recipe['nutritions'] = {}
+	for item in list(zip(soup.select(".nutritions dl")[0].find_all("dt"), soup.select(".nutritions dl")[0].find_all("dd"))):
+		dt, dl = item
+		recipe['nutritions'].update({ dt.string.replace('\n','').strip().lower(): re.sub(r'\s{2,}', ' ', dl.string.replace('\n','').strip().lower()) })
 	recipe['steps'] = [re.sub(' +', ' ', li.text).replace('\n','').strip() for li in soup.select("#preparation-steps li")]
+	recipe['tags'] = [a.text.replace('#','').replace('\n','').strip().lower() for a in soup.select(".core-tags-wrapper__tags-container a")]
 
 	return recipe
 
